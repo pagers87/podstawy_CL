c479fe1e02d59e5c2ceffdb0f7256cd578d23014
Piotr Grzela <piotr.grzela@op.pl>
2023-03-17 00:16:52 +0100

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,21 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 10;
+        int b = 3;
+        double c = 12, d =15;
+         System.out.println(a / (double) b);
+
+         if (a == 10) {
+             System.out.println("dipa");
+         } else if (b <= 4) {
+             System.out.println("b jest większa hahha");
+         } else  {
+             System.out.println("Ala ma kota");
+         }
+
+
+        // System.out.println("Hello world!");
     }
 }
