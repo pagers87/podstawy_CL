dd775445c24802b033598f8d8b47ef5fff822a7c
Piotr Grzela <piotr.grzela@op.pl>
2023-03-17 00:16:52 +0100

    zadanie dot. kontroli wieku z działu instrukcje warunkowe - kurs udemy.com

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,21 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int wiek = 15;
+
+        if (wiek <= 15 )
+        {
+            System.out.println("Musisz się uczyć");
+        }
+
+        else if (wiek >= 18 && wiek <= 65)
+        {
+            System.out.println("Możesz pracować");
+        }
+        else if ( wiek >= 65) {
+            System.out.println("Możesz iść na emeryturę");
+        }
+
     }
 }
