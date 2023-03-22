0c5973bbf79fe70e89225dcc60d11dfb1729e74a
Piotr Grzela <piotr.grzela@op.pl>
2023-03-22 21:25:25 +0100

    tablice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,20 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int wiek = 15;
 
-        if (wiek <= 15 )
-        {
-            System.out.println("Musisz się uczyć");
-        }
+    int [] [] tab = new int[4][3];
+    tab [0][0] = 25;
+    tab [3][2] = 70;
 
-        else if (wiek >= 18 && wiek <= 65)
-        {
-            System.out.println("Możesz pracować");
-        }
-        else if ( wiek >= 65) {
-            System.out.println("Możesz iść na emeryturę");
-        }
+        System.out.println(tab[3][2]);
 
+        int[][] tab2 =
+                {
+                        {4, 15, 67},
+                        {677,887,66},
+                        {656,876,22},
+                        {655,899,999}
+                };
+        System.out.println(tab2[3][0]);
+
+        }
     }
-}
