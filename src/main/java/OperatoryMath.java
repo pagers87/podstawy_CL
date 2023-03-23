58bed49c6cbe42b74c25cf73f2be6c71431c7040
Piotr Grzela <piotr.grzela@op.pl>
2023-03-23 22:00:04 +0100

    OperatoryMath.java

---
+++ src/main/java/OperatoryMath.java
@@ -0,0 +1,20 @@
+public class OperatoryMath {
+    public static void main(String[] args) {
+        int firstnum = 4;
+        int secondnum = 6;
+        float thirdnum = 4.0f;
+
+        int addition = firstnum + secondnum;
+        int subtraction = firstnum - secondnum;
+        int multiplication = firstnum * secondnum;
+        float division = thirdnum / secondnum;
+        int mod = secondnum % firstnum;
+
+        System.out.println(addition);
+        System.out.println(subtraction);
+        System.out.println(multiplication);
+        System.out.println(division);
+        System.out.println(mod);
+
+    }
+}
